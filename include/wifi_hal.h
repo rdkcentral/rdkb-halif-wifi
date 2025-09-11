edd2d24c269e42710f43a6cd781d512f8d264b3d
Oleh Hirniak <Oleh_Hirniak@comcast.com>
2025-09-11 12:03:04 +0300

    Move headers from gerrit stable2 branch.

--- include/wifi_hal.h
+++ include/wifi_hal.h
@@ -110,12 +110,9 @@
 * Includes API's used by Plume Mesh.
  */
 #include "wifi_hal_extender.h"
-
-//TODO: This header file needs to be corrected, some API's defined in it are currently in use, and require correction or removal.
 /**
 * Wifi deprecated APIs.
 * APIs to be deprecated. Not add new function or structure!
 */
 #include "wifi_hal_deprecated.h"
-
 #endif
