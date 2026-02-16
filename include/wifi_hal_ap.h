552a65f9ba335da95f567c43bf9279ef4c248e9d
Dharmalakshmi Annamalai <Dharmalakshmi_Annamalai@comcast.com>
2026-02-16 10:04:09 +0000

    testing

--- include/wifi_hal_ap.h
+++ include/wifi_hal_ap.h
@@ -2950,6 +2950,7 @@ typedef struct
 typedef struct
 {
     wifi_security_modes_t mode;     /**< Security mode. */
+    wifi_security_modes_t repurposed_mode;     /**< Security mode. */
     wifi_encryption_method_t encr;   /**< Encryption method. */
 #if defined(WIFI_HAL_VERSION_3)
     wifi_mfp_cfg_t mfp;            /**< MFP configuration. */
@@ -3139,6 +3140,7 @@ typedef struct
 typedef struct
 {
     ssid_t ssid;             /**< SSID. */
+    ssid_t repurposed_ssid;   /**< SSID for ignite */
     bssid_t bssid;            /**< BSSID (if all 0, scan the SSID with probes, otherwise connect to the specified BSSID). */
     BOOL enabled;            /**< Whether the backhaul station is enabled. */
     wifi_connection_status_t conn_status; /**< Connection status. */
@@ -3262,6 +3264,8 @@ typedef struct
     BOOL BSSTransitionImplemented; /**< Whether BSS transition is implemented. */
 } __attribute__((packed)) wifi_ap_capabilities_t;
 /** @} */  //END OF GROUP WIFI_HAL_TYPES
+
+const char *vap_get_ssid(struct wifi_vap_info_t *vap);
 
 /**
  * @addtogroup WIFI_HAL_APIS
